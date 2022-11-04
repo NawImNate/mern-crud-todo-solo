fe3555d7ee9c3f233b6e840a66b979ff7b1b2ad2
nawimnate <jnhatch3@gmail.com>
2022-11-04 12:42:20 -0400

    created server, connected to mdb

--- server/server.js
+++ server/server.js
@@ -1,3 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const app = express();
+
+const port = 5000;
+const dotenv = require("dotenv");
+
+app.use(express.json());
+
+// environment variables
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Listen on port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// Connect to mdb
+mongoose.connect(
+  // "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@todo.h8eo3xa.mongodb.net/test",
+  "mongodb+srv://admin:kep96xOejv6O2BtF@todo.h8eo3xa.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
